80966e9a7cea09b73dd2256b19ffa7a0443780dc
Roy Josefsson <roy.josefsson@gmail.com>
2023-08-11 19:59:42 +0200

    refactor - adjust for stronger typing and consistency

--- repo/index.ts
+++ repo/index.ts
@@ -1,20 +1,14 @@
 import { OMDBAPI_URL, OMDBAPI_API_KEY, SWAPI_URL } from "@/data/site-settings"
 import { JoinedMovie, OmdbapiMovie, SwapiMovie } from "@/interfaces/movie"
-import { convertToRoman } from "@/utils/convertToRoman"
-
-interface MoviesResponse {
-    results: SwapiMovie[]
-}
+import { withExtendedTitle } from "@/utils/withExtendedTitle"
 
 const Swapi = {
     movies: {
         fetch: async (): Promise<SwapiMovie[]> => {
             const response = await fetch(SWAPI_URL)
-            const data: MoviesResponse = await response.json()
-            return data.results.map(x => ({
-                ...x,
-                title: `Episode ${convertToRoman(x.episode_id)} - ${x.title}`
-            }))
+            const data = await response.json()
+            const movies: SwapiMovie[] = data.results.map(withExtendedTitle)
+            return movies
         }
     }
 }
@@ -23,25 +17,23 @@ const Omdbapi = {
     data: {
         fetch: async (title: string): Promise<OmdbapiMovie> => {
             const response = await fetch(`${OMDBAPI_URL}/?apikey=${OMDBAPI_API_KEY}&t=${encodeURIComponent(title)}`)
-            const data = await response.json()
+            const data: OmdbapiMovie = await response.json()
             return data
         }
     }
 }
 
-const getPromises = async (swapiMovie: SwapiMovie) => {
+const getPromises = async (swapiMovie: SwapiMovie): Promise<JoinedMovie> => {
     const omdbapiMovie = await Omdbapi.data.fetch(swapiMovie.title)
-    return {
-        swapi: swapiMovie,
-        omdbapi: omdbapiMovie,
-    }
+    const joinedMovie: JoinedMovie = { swapi: swapiMovie, omdbapi: omdbapiMovie, }
+    return joinedMovie
 }
 
 const joinedMovies = {
     movies: {
         fetch: async (): Promise<JoinedMovie[]> => {
-            const movies = await Swapi.movies.fetch();
-            const joinedMoviesData = await Promise.all(movies.map(getPromises));
+            const swapiMovies = await Swapi.movies.fetch();
+            const joinedMoviesData = await Promise.all(swapiMovies.map(getPromises));
             return joinedMoviesData;
         },
     },
